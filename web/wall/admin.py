0022fe70a05efc49a1c0624bb50bbca79e151179
Stef Pletinck <s@stefpletinck.be>
2017-12-09 18:21:11 +0100

    Admin for sms

--- web/wall/admin.py
+++ web/wall/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Picture, Blocked_Poster
+from .models import Picture, Blocked_Poster, Sms, Blocked_Number
 
 
 admin.site.register(Picture)
 admin.site.register(Blocked_Poster)
+admin.site.register(Sms)
+admin.site.register(Blocked_Number)
