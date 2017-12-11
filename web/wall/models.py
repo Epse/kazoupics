658ecbc9dfdbefd795b777afe03d724c66a9c5b2
Stef Pletinck <s@stefpletinck.be>
2017-12-11 09:53:19 +0100

    Auto-delete posts by blocked users

--- web/wall/models.py
+++ web/wall/models.py
@@ -10,11 +10,18 @@ class Picture(models.Model):
 
 
 class Blocked_Poster(models.Model):
-    name = models.CharField(max_length=255, db_index=True, blank=True, null=True)
-    ip = models.CharField(max_length=100, db_index=True, blank=True, null=True)
+    name = models.CharField(max_length=255, db_index=True, blank=True, null=False)
+    ip = models.CharField(max_length=100, db_index=True, blank=True, null=False)
 
     def save(self, *args, **kwargs):
-        self.name = self.name.lower()
+        if self.name is not None:
+            self.name = self.name.lower()
+            for pic in Picture.objects.filter(poster__iexact=self.name):
+                pic.delete()
+
+        if self.ip is not None:
+            for pic in Picture.objects.filter(ip=self.ip):
+                pic.delete()
         return super(Blocked_Poster, self).save(*args, **kwargs)
 
 
@@ -26,3 +33,8 @@ class Sms(models.Model):
 
 class Blocked_Number(models.Model):
     number = models.CharField(max_length=20, blank=False, null=False)
+
+    def save(self, *args, **kwargs):
+        for sms in Sms.objects.filter(sender=self.number):
+            sms.delete()
+        return super(Blocked_Number, self).save(*args, **kwargs)
