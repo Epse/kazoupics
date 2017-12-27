714b8ce251be5820122937d3fdc75913a9580fe2
Stef Pletinck <s@stefpletinck.be>
2017-12-27 14:08:15 +0100

    added manage script

---
+++ manage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function up {
+    docker-compose up -d
+}
+
+function setup {
+    docker-compose up -d --build
+    docker-compose exec web ./manage.py createsuperuser
+}
+
+function stop {
+    docker-compose stop
+}
+
+function clean {
+    docker-compose down
+    docker-compose rm -a
+}
