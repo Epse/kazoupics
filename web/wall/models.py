9bd3a7b34e9814d4a2ad255f91c415351c86e0dc
Stef Pletinck <s@stefpletinck.be>
2018-10-04 07:28:40 +0200

    added delete propertie and handlers

--- web/wall/models.py
+++ web/wall/models.py
@@ -8,6 +8,7 @@ class Picture(models.Model):
     ip = models.CharField(max_length=100)
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
     is_ad = models.BooleanField(default=False)
+    deleted = models.BooleanField(default=False, db_index=True)
 
     def __str__(self):
         return 'Picture by ' + self.poster
@@ -21,7 +22,8 @@ class Blocked_Poster(models.Model):
         if self.name is not None:
             self.name = self.name.lower()
             for pic in Picture.objects.filter(poster__iexact=self.name):
-                pic.delete()
+                pic.deleted = True
+                pic.save()
 
         if self.ip is not None:
             for pic in Picture.objects.filter(ip=self.ip):
@@ -40,6 +42,7 @@ class Sms(models.Model):
     sender = models.CharField(max_length=20, blank=False, null=False)
     text = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
+    deleted = models.BooleanField(default=False, db_index=True)
 
     def __str__(self):
         return self.sender + ': ' + self.text[:10]
@@ -50,7 +53,8 @@ class Blocked_Number(models.Model):
 
     def save(self, *args, **kwargs):
         for sms in Sms.objects.filter(sender=self.number):
-            sms.delete()
+            sms.deleted = True
+            sms.save()
         return super(Blocked_Number, self).save(*args, **kwargs)
 
     def __str__(self):
